01fc30948cd6cdf23973fc1ee1e7818c6d2c6ad6
usbbush <jeanbienvenusb@gmail.com>
2024-07-12 11:58:12 +0200

    Generate model for article and search

---
+++ db/migrate/20240712093305_create_searches.rb
@@ -0,0 +1,11 @@
+class CreateSearches < ActiveRecord::Migration[7.1]
+  def change
+    create_table :searches do |t|
+      t.string :query
+      t.integer :count
+      t.integer :result_count
+
+      t.timestamps
+    end
+  end
+end
