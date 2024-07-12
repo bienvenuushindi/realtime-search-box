a1fb768a4152a32d3519fbd917eaa457e78eae1f
usbbush <jeanbienvenusb@gmail.com>
2024-07-12 18:39:02 +0200

    Add seeder for articles table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Article.delete_all
+
+# Generate 100 articles
+titles = Set.new
+while titles.size < 100
+  title = Faker::Movie.unique.title
+  titles.add(title)
+end
+titles.each do |title|
+  Article.create!(
+    title: title,
+  )
+end
+
+puts "Seeded 100 articles"
