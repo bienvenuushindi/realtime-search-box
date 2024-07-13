da68ee127c2b3d070f84e69ff4c72c79c2cf42d5
usbbush <jeanbienvenusb@gmail.com>
2024-07-13 07:44:36 +0200

    Create a queue for queries segments

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
-
 class SearchService
-
+  class << self
+    def push_to_redis(redis, query)
+      redis.lpush(
+        "ip:#{query[:ip]}",
+        {
+          query: query[:value],
+          created_at: DateTime.now.strftime('%Q')
+        }.to_json
+      )
+    end
+  end
 end
